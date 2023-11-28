225ad6c809fd4c4013e3171a300d7b6e160deaad
reznuhasan <reznuhasan31@gmail.com>
2023-11-28 08:42:10 +0600

    add doctor design finish

--- hospital-management-project/frontend/src/Pages/AdminHeader.jsx
+++ hospital-management-project/frontend/src/Pages/AdminHeader.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FiAlignJustify } from "react-icons/fi";
 
 const AdminHeader = () => {
   return (
-    <div>AdminHeader</div>
+    <div>
+      <FiAlignJustify />
+
+    </div>
   )
 }
 
