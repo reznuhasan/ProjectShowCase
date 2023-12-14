2c60af2e3206e8a10a13140a3143b5399bbc784c
reznuhasan <reznuhasan31@gmail.com>
2023-12-14 19:22:36 +0600

    appointment error handle

--- hospital-management-project/backend/src/controllers/appointment.ctrl.js
+++ hospital-management-project/backend/src/controllers/appointment.ctrl.js
@@ -2,8 +2,15 @@ import Appointment from "../models/appointment.model.js";
 
 const createAppointment=async(req,res)=>{
     try {
+        const findAppointment=await Appointment.find({email:req.body.email})
+        const {doctor,month,date,name}=req.body
+       const checkAppointment= findAppointment.filter(appointment=>appointment.name===name && appointment.doctor===doctor && appointment.month===month && appointment.date===date)
+       console.log(checkAppointment.length)
+       if(checkAppointment.length>0){
+        return res.status(409).json({error:"already submit a appointment"})
+       }
         const appointment=new Appointment(req.body);
-        const savedAppointment =await appointment.save();
+        const savedAppointment =await appointment.save();  
         return res.status(200).json({message:"appointment request submit",savedAppointment})
     } catch (error) {
         return res.status(500).json({ error: "Internal Server Error" });
