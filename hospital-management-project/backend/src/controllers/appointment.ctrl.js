c6d94ab68ebbc1dab182aa20eac47fc6c1c9ccaa
reznuhasan <reznuhasan31@gmail.com>
2023-12-16 16:11:16 +0600

    doctor approve part

--- hospital-management-project/backend/src/controllers/appointment.ctrl.js
+++ hospital-management-project/backend/src/controllers/appointment.ctrl.js
@@ -1,5 +1,6 @@
+import mongoose from "mongoose";
 import Appointment from "../models/appointment.model.js";
-
+import Doctor from "../models/doctor.model.js";
 const createAppointment=async(req,res)=>{
     try {
         const findAppointment=await Appointment.find({email:req.body.email})
@@ -20,7 +21,35 @@ const findAllAppointment=async(req,res)=>{
         const allAppointments = await Appointment.find()
         return res.status(200).json({"message":"get all appointment",allAppointments})
     } catch (error) {
-        return res.status(500).json({ error: "Internal Server Error" });
+        return res.status(500).json({ error: "Internal Server Error"});
     }
 }
-export {createAppointment,findAllAppointment}
+const updateAppointment = async (req, res) => {
+    try {
+      console.log(req.body.id)
+      console.log(req.body.doctorName);
+      const appointment = await Appointment.findById(req.body.id);
+      if (!appointment) {
+        return res.status(404).json({ error: "Appointment not found" });
+      }
+    //   check doctor
+      const doctor=await Doctor.findOne({name:req.body.doctorName})
+      if(!doctor){
+        return res.status(404).json({ error: "doctor not found" });
+      }
+      //check approveval status
+      if(appointment.status==="approve"){
+        return res.status(409).json({error:"Appointment already approved"})
+      }
+      doctor.serial=doctor.serial-1;
+      appointment.status="approve";
+      console.log(doctor);
+      console.log(appointment);
+      await doctor.save();
+     const savedAppointment= await appointment.save();
+      return res.status(200).json({ message: "update appointment",savedAppointment});
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+export {createAppointment,findAllAppointment,updateAppointment}
--- hospital-management-project/backend/src/routes/appointment.route.js
+++ hospital-management-project/backend/src/routes/appointment.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createAppointment, findAllAppointment } from "../controllers/appointment.ctrl.js";
+import { createAppointment, findAllAppointment, updateAppointment } from "../controllers/appointment.ctrl.js";
 
 const router=express.Router();
 
 router.post('/',createAppointment)
 router.get("/all",findAllAppointment)
+router.patch("/update",updateAppointment)
 
 export default router;
