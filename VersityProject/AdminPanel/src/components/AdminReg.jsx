4241ea42e4e5952c709daba6509c4c87fa483856
reznuhasan <reznuhasan31@gmail.com>
2023-12-17 21:15:10 +0600

    admin section added

--- VersityProject/AdminPanel/src/components/AdminReg.jsx
+++ VersityProject/AdminPanel/src/components/AdminReg.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import Styles from "../styles/AdminReg.module.css"
 const AdminReg = () => {
   return (
     <div>
-      <form action="">
+      <form>
         
+        <div className={Styles.adminReg}>
+        <div>
+          <h2>Register Admin</h2>
+          <label>Full Name</label>
+          <input type="text" placeholder="Enter Your Full Name"/>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" placeholder="Enter Your Email" />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input type="password" placeholder="Enter Your Password"/>
+        </div>
+        
+        <div>
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Confirm Your Password" />
+        </div>
+        <div>
+          <button type="button">Register</button>
+        </div>
+        </div>
+
+
       </form>
     </div>
   )
